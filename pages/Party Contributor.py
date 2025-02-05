69e8cfd8a70ff1f6766fd844a784778f58636a5e
VictoriousMango <additya.mishra20@st.niituniversity.in>
2025-02-05 17:03:21 +0530

    Introducing Python Files in msin brach

---
+++ pages/Party Contributor.py
@@ -0,0 +1,113 @@
+import streamlit as st
+import smtplib
+import pandas as pd
+
+class Distributor():
+    def __init__(self):
+        # self.numberOfPeople = numberOfPeople
+        # self.TotalAmount = TotalAmount
+        self.Log = pd.DataFrame(columns=['Who Paid', 'Item', 'How much Paid', 'Contributors'])
+        self.BalanceTable = pd.DataFrame(columns=['Name', 'Have Spent', 'Will Receive', 'Net Total'])
+        self.BalancePerPerson = {'Name':[], 'Have Spent':[], 'Will Receive':[], 'Net Total':[]}
+    
+    def entryBalancePerPerson(self, Name, Spent, Receive):
+        # print(Name, Spent, Receive)
+        #If the Name already made an entry
+        #Else: to create a new entry
+        if Name in self.BalancePerPerson['Name']:
+            self.BalancePerPerson['Have Spent'][self.BalancePerPerson['Name'].index(Name)] += Spent
+            self.BalancePerPerson['Will Receive'][self.BalancePerPerson['Name'].index(Name)] += Receive
+            self.BalancePerPerson['Net Total'][self.BalancePerPerson['Name'].index(Name)] = self.BalancePerPerson['Have Spent'][self.BalancePerPerson['Name'].index(Name)] - self.BalancePerPerson['Will Receive'][self.BalancePerPerson['Name'].index(Name)]
+        else:
+            self.BalancePerPerson['Name'].append(Name)
+            self.BalancePerPerson['Have Spent'].append(Spent)
+            self.BalancePerPerson['Will Receive'].append(Receive)
+            self.BalancePerPerson['Net Total'].append(Spent-Receive)
+    
+    def EqualDistributor(self, TotalAmount, numberOfPeople):
+        return TotalAmount/numberOfPeople
+    
+    def log(self, entryNumber):
+        col1, col2, col3, col4, col5 = st.columns([1, 1, 1, 1, 1]) 
+        # Used for Logging
+        self.Log.loc[len(self.Log.index)] = [
+            col1.text_input(f"Who has Paid - Transaction:{entryNumber}"), 
+            col2.text_input(f"Item Paid For - Transaction:{entryNumber}"),
+            col3.number_input(f"How Much Paid - Transaction:{entryNumber}"), 
+            [i.strip() for i in col4.text_input(f"Contributors - Transaction:{entryNumber}").split(',')]
+            ]
+        
+        # Entry in Balance Sheet
+        self.entryBalancePerPerson(
+            Name=self.Log.loc[len(self.Log.index) - 1][0],
+            Spent=self.Log.loc[len(self.Log.index) - 1][2], 
+            Receive=self.Log.loc[len(self.Log.index) - 1][2] - self.EqualDistributor(self.Log.loc[len(self.Log.index) - 1][2], len(self.Log.loc[len(self.Log.index) - 1][3])+1 )
+        )
+        for i in self.Log.loc[len(self.Log.index) - 1][3]:
+            self.entryBalancePerPerson(
+                Name=i, 
+                Spent=self.EqualDistributor(self.Log.loc[len(self.Log.index) - 1][2], len(self.Log.loc[len(self.Log.index) - 1][3]) + 1),
+                Receive=0
+            )
+
+        if col5.select_slider(f"Log Transaction:{entryNumber+1}", options=['No', 'Yes']) == 'Yes':
+            entryNumber+=1
+            self.log(entryNumber)
+    
+    
+    def BalanceSheet(self):
+        Names = set(i for i in self.Log["Who Paid"])
+        BalancePerPerson = {
+            'Name': [i for i in self.BalancePerPerson['Name']],
+            'Have Spent': [i for i in self.BalancePerPerson['Have Spent']],
+            'Will Receive': [i for i in self.BalancePerPerson['Will Receive']],
+            'Net Total': [i for i in self.BalancePerPerson['Net Total']]
+        }
+
+        return self.BalancePerPerson
+
+    def NonLinerDistribution(self):
+        self.log(1)
+        return self.Log
+
+
+class Email ():
+    # Constructor Class to construct all the required variables.
+    def init(self, Requirements, message):
+        self.sender = Requirements[0]
+        self.password = Requirements[1]
+        self.receiver = Requirements[2]
+        self.message = message
+        
+    def SendEmail(self):
+        with st.sidebar:
+            server = smtplib.SMTP("smtp.gmail.com", 587)
+            st.info(f'Echoing server: {server.ehlo()}')
+            st.info(f'Starting TLS Connection: {server.starttls()}')
+            st.info(f'Logging in: {server.login(self.sender, self.password)}')
+            server.sendmail(self.sender, self.receiver, self.message)
+            print(f"Email Sent to: {self.receiver}")
+        
+st.title("Party Contributor!!!")
+# print('########################## New Run')
+# distributor = [
+#     st.number_input("Enter the Total Amount:"),
+#     st.number_input("Number of people:")
+# ]
+
+
+
+# if st.button("Calculate Equal Distribution"):
+#     with st.spinner("Generating per person Contribution!!!"):
+#         message = Distributor(numberOfPeople=distributor[1], TotalAmount=distributor[0]).EqualDistributor()
+#     st.success(message)
+
+with st.expander("Log Distribution"):
+    with st.spinner("Generating per person Contribution!!!"):
+        Party = Distributor()
+        Log = Party.NonLinerDistribution()
+st.table(Log)
+
+with st.expander("Contribution per person Generated"):
+    st.table(Party.BalanceSheet())
+    
---
+++ pages/Password Generator.py
@@ -0,0 +1,36 @@
+import streamlit as st
+import random
+
+smallAlphabet = [chr(i) for i in range(97, 123)]
+CapitalAlphabet = [chr(i) for i in range(65, 91)]
+specialCharacters = [chr(i) for i in range(33, 48)] + \
+                          [chr(i) for i in range(58, 65)] + \
+                          [chr(i) for i in range(91, 97)] + \
+                          [chr(i) for i in range(123, 127)]
+pwdLength = int(st.number_input("Number characters in pwd") // 1)
+st.info("Entered string will be treated as a single character for password generation")
+Strings = st.text_input("Enter string to include(separate with commas if multiple strings)").split(",")
+Strings = [i.strip() for i in Strings]
+
+setToUse = {
+    1 : Strings,
+    2 : smallAlphabet,
+    3 : CapitalAlphabet,
+    4 : specialCharacters
+}
+
+with st.expander("Suggested Passwords"):
+    pwd = ["" for i in range(5)]
+    for i in range(5):
+        setInUse = 0
+        for j in range(pwdLength):
+            setInUse = random.randint(1, len(setToUse.keys()))                     ### To select a set randomly
+            try: 
+                pwd[i] += setToUse[setInUse][random.randint(1, len(setToUse[setInUse]) - 1)] ### To select an Element randomly and show the password.
+            ### Subtracting 1, as the len is last index + 1
+            except: ### as the Strings variable can only contain one variable
+                 pwd[i] += setToUse[setInUse][0]
+            finally: ### If nothing is entered in Strings
+                pass
+    st.write(pwd)
+    #st.write(setToUse)
